efa66b154b54534c5ae35050f041ef0fc9bf2c6e
Tianqi Zhang <1120404010@qq.com>
2015-09-14 09:58:24 +0800

    Initial Commit

---
+++ lesson_11/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lesson_11
+//
+//  Created by 张天琦 on 15/9/14.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lesson_11/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lesson_11
+//
+//  Created by 张天琦 on 15/9/14.
+//  Copyright (c) 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
